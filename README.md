7071ee4a47bc947175262db3a9a1da31bfed3a5a
Rik <rik@pqina.nl>
2023-05-22 15:52:04 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Pintura React TypeScript example project
+
+Run `npm install` to install project dependencies, then run `npm start` to start the development server.
+
+Navigate to http://localhost:3000 to view the project.
+
+Or try it out right now and [open the project on StackBlitz](https://stackblitz.com/github/pqina/pintura-example-react-typescript)
+
+### License
+
+This projects uses a test version of Pintura. This version of Pintura will show a watermark in the editor and on generated images.
+
+Purchase a license at https://pqina.nl/pintura to use Pintura in production.
---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Example from "./Example";
+import ExampleModal from "./ExampleModal";
+import ExampleOverlay from "./ExampleOverlay";
+import ExampleFilePond from "./ExampleFilePond";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h1>Pintura Image Editor</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Example</Link>
+            </li>
+            <li>
+              <Link to="/modal">Modal</Link>
+            </li>
+            <li>
+              <Link to="/overlay">Overlay</Link>
+            </li>
+            <li>
+              <Link to="/filepond">FilePond</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/modal">
+            <ExampleModal />
+          </Route>
+          <Route path="/overlay">
+            <ExampleOverlay />
+          </Route>
+          <Route path="/filepond">
+            <ExampleFilePond />
+          </Route>
+          <Route path="/">
+            <Example />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
